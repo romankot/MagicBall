7ba26b400d2eba141baba5fad588f1f0a32ad520
r.kot <kremor@gmail.com>
2021-10-19 16:26:49 +0300

    shake handler added

--- MagicBall/ViewController.swift
+++ MagicBall/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let answers = ["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy, try again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"]
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var shakeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func shakeButtonPressed(_ sender: UIButton){
+        let randomIndex = Int.random(in: 0..<answers.count)
+        print("Shake it like a polaroid picture!")
+        answerLabel.text = answers[randomIndex]
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        guard motion == .motionShake else { return }
+        let randomIndex = Int.random(in: 0..<answers.count)
+        answerLabel.text = answers[randomIndex]
+    }
 
 }
 
